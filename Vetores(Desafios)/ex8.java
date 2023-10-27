93e5ca30894af833aef49ae7af7119778f000724
Cadu_JVN <145131105+cadu01234@users.noreply.github.com>
2023-10-27 11:17:51 -0300

    Exercício 8
    Ler 10 elementos inteiros em um vetor A. Montar uma rotina de busca, para
    pesquisar se um dado elemento X está armazenado em A.

---
+++ Vetores(Desafios)/ex8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ex8 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		final int TAM=5;
+		int a[], i, j, comp;
+		a = new int[TAM];
+		for(i=0;i<TAM; i++) {
+			System.out.println("Forneça o " +(i+1)+ "° valor do vetor A");
+			a[i] = in.nextInt();
+		}
+		System.out.println("Qual número deseja buscar");
+		comp = in.nextInt();
+		for (j=0; j<TAM; j++) {
+		if (comp==a[j]) {
+			System.out.println("O número que você buscou esta em: A[" + j + "] = " + a[j]);
+		}else {
+			System.out.println("Número não encontrado");
+		}
+		
+	}
+	}
+}
