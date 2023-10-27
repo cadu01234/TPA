6427645ebba45f2158c26c84f2edd7a2313c3a24
Cadu_JVN <145131105+cadu01234@users.noreply.github.com>
2023-10-27 08:46:45 -0300

    Exercício 1
    Ler um vetor A com 20 elementos. Separar os elementos pares e ímpares de
    A utilizando apenas um vetor extra B. Sugestão: no início do vetor B
    armazene os elementos pares de A e nas posições restantes do vetor B
    armazene os elementos de A que são ímpares.

---
+++ Vetores(Desafios)/ex1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ex1 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		final int TAM = 8;
+		int a[], b[], i, l, j=0, k=0, p=0, m=0, result;
+		a = new int [TAM];
+		b = new int [TAM];
+		
+		for (i= 0; i < TAM; i++) {
+			System.out.println("Digite o " + (i+1) + "° valor do vetor A");
+			a[i] =in.nextInt();
+			if (a[i] % 2 == 0) {
+			    b[p] = a[i];
+			    p++;
+			 } else {
+	                b[TAM - 1 - m] = a[i];
+	                m++;
+	         }
+		
+			
+		
+	}
+		for (l = 0; l < TAM; l++) {
+		    System.out.print(b[l] + " ");
+		}
+}
+}
