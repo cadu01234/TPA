ac692f157c35972b8513881a75d6f5786bcc6219
Cadu_JVN <145131105+cadu01234@users.noreply.github.com>
2023-10-27 08:52:58 -0300

    Exercício 5
    Criar um vetor A com 15 elementos inteiros. Construir um vetor B de
    mesmo tamanho, sendo que cada elemento do vetor B seja o fatorial do
    elemento correspondente em A.

---
+++ Vetores(Desafios)/ex5.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class ex5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int TAM = 5;
+        int[] A = new int[TAM];
+        int[] B = new int[TAM];
+        
+
+        for (int i = 0; i < TAM; i++) {
+            System.out.print("Digite o " + (i + 1) + "° valor do vetor A: 3");
+            A[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < TAM; i++) {
+            B[i] = cf(A[i]);
+        }
+
+        System.out.println("Vetor B");
+        for (int i = 0; i < TAM; i++) {
+            System.out.println("B[" + i + "] = " + B[i]);
+        }
+    }
+
+    public static int cf(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int fatorial = 1;
+        for (int i = 1; i <= n; i++) {
+            fatorial *= i;
+        }
+        return fatorial;
+    }
+}
